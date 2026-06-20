cdc5c552bd3387b4d905b721487bfcfdeda75056
DaxPay Dev <dev@daxpay.com>
2026-06-20 10:21:05 +0800

    fix 切换登陆方式白屏问题

--- apps/daxpay-admin/src/layouts/authentication/form.vue
+++ apps/daxpay-admin/src/layouts/authentication/form.vue
@@ -13,16 +13,14 @@
     <slot></slot>
     <!-- Router View with Transition and KeepAlive -->
     <RouterView v-slot="{ Component, route }">
-      <Transition appear mode="out-in" name="slide-right">
-        <KeepAlive :include="['Login']">
-          <component
-            :is="Component"
-            :key="route.fullPath"
-            class="side-content mt-6 w-full sm:mx-auto md:max-w-md"
-            :data-side="dataSide"
-          />
-        </KeepAlive>
-      </Transition>
+      <KeepAlive :include="['Login']">
+        <component
+          :is="Component"
+          :key="route.fullPath"
+          class="side-content mt-6 w-full sm:mx-auto md:max-w-md"
+          :data-side="dataSide"
+        />
+      </KeepAlive>
     </RouterView>
 
     <!-- Footer Copyright -->
